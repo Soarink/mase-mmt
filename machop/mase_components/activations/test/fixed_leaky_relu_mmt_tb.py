a30020b11bf0b8fe3a968748f338f0da7935c911
Zixian Jin <119010134@link.cuhk.edu.cn>
2024-02-12 16:47:59 +0000

    fixed_leaky_relu_mmt and its testbench

--- machop/mase_components/activations/test/fixed_leaky_relu_mmt_tb.py
+++ machop/mase_components/activations/test/fixed_leaky_relu_mmt_tb.py
@@ -5,12 +5,19 @@ from torch.autograd.function import InplaceFunction
 
 import cocotb
 
-from cocotb.triggers import Timer
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, Timer
+
+import os
+import random
+from pathlib import Path
 
 import pytest
-import cocotb
 
-from mase_cocotb.runner import mase_runner
+import cocotb
+from cocotb.clock import Clock
+from cocotb.runner import get_runner
+from cocotb.triggers import FallingEdge
 
 pytestmark = pytest.mark.simulator_required
 
@@ -56,15 +63,17 @@ class VerificationCase:
     bitwidth = 32
     bias = 4
     num = 16
-
+    bias = 4
+    
     def __init__(self, samples=2):
-        self.m = nn.ReLU()
+        self.m = nn.LeakyReLU(negative_slope=0.5**3)
         self.inputs, self.outputs = [], []
         for _ in range(samples):
             i, o = self.single_run()
             self.inputs.append(i)
             self.outputs.append(o)
         self.samples = samples
+
 
     def single_run(self):
         xs = torch.rand(self.num)
@@ -78,6 +87,7 @@ class VerificationCase:
         return {
             "IN_SIZE": self.num,
             "IN_WIDTH": self.bitwidth,
+            # "BIAS": self.bias
         }
 
     def get_dut_input(self, i):
@@ -87,12 +97,20 @@ class VerificationCase:
 
     def get_dut_output(self, i):
         outputs = self.outputs[i]
-        shifted_integers = (outputs * (2**self.bias)).int() - 1
+        # print('outputs= ', outputs.numpy().tolist())
+        shifted_integers = (outputs * (2**self.bias))
+        # print(shifted_integers)
+        for j in range(len(shifted_integers)):
+            tmp = shifted_integers[j]
+            if (tmp < 0) and (int(tmp) != tmp):
+                shifted_integers[j] -= 1
+        # print(shifted_integers)
+        shifted_integers = shifted_integers.int()
         return shifted_integers.numpy().tolist()
 
 
 @cocotb.test()
-async def test_leaky_fixed_relu(dut):
+async def test_fixed_leaky_relu_mmt(dut):
     """Test integer based Relu"""
     test_case = VerificationCase(samples=100)
 
@@ -100,12 +118,47 @@ async def test_leaky_fixed_relu(dut):
     for i in range(test_case.samples):
         x = test_case.get_dut_input(i)
         y = test_case.get_dut_output(i)
-
+        # print('x= ', x)
+        # print('y= ', y)
         dut.data_in.value = x
         await Timer(2, units="ns")
-        assert dut.data_out.value == y, f"output q was incorrect on the {i}th cycle"
+        # print(x)
+        # for i in range(len(y)):
+        #     print('HW=', dut.data_out.value[i], ',\tSW=', y[i], ',\tResult=', dut.data_out.value[i]==signed_to_unsigned([y[i]])[0])
+        assert dut.data_out.value == signed_to_unsigned(y), f"output q was incorrect on the {i}th cycle"
+
+
+def signed_to_unsigned(x):
+    ''' bitwidth=32 by default'''
+    x_unsigned = []
+    for e in x:    
+        if e >= 0:
+            x_unsigned.append(e)
+        else:
+            x_unsigned.append(2**32 - abs(e))
+    return x_unsigned
+    
+def runner():
+    sim = os.getenv("SIM", "verilator")
+    verilog_sources = []
+
+    verilog_sources = ["../../../../components/activations/fixed_leaky_relu_mmt.sv"]
+    test_case = VerificationCase()
+
+    # set parameters
+    extra_args = []
+    for k, v in test_case.get_dut_parameters().items():
+        extra_args.append(f"-G{k}={v}")
+    print(extra_args)
+    runner = get_runner(sim)
+    runner.build(
+        verilog_sources=verilog_sources,
+        hdl_toplevel="fixed_leaky_relu_mmt",
+        build_args=extra_args,
+    )
+
+    runner.test(hdl_toplevel="fixed_leaky_relu_mmt", test_module="fixed_leaky_relu_mmt_tb")
 
 
 if __name__ == "__main__":
-    tb = VerificationCase()
-    mase_runner(module_param_list=[tb.get_dut_parameters()])
+    runner()
